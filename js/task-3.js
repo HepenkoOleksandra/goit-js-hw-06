8dbae4f1eb33ad49b56e7526a6f7c91317f23651
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-01-01 14:37:53 +0200

    corection two

--- js/task-3.js
+++ js/task-3.js
@@ -12,15 +12,15 @@ class StringBuilder {
     }
 
     padStart(str) {
-        this.#value = str + this.#value;
+        this.#value = `${str + this.#value}`;
     }
 
     padEnd(str) {
-        this.#value = this.#value + str;
+        this.#value = `${this.#value + str}`;
     }
     
     padBoth(str) {
-        this.#value = str + this.#value + str;
+        this.#value = `${str + this.#value + str}`;
     }
 }
 
